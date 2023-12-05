186d84bd32c5fe835caa775f855f9e2fd64cd194
Erik Blücher <erik.blucher@gmail.com>
2023-12-05 22:28:25 +0100

    2023: Day 5. Slow part 2!

---
+++ 2023/5/Mapping.cs
@@ -0,0 +1,8 @@
+﻿namespace _5
+{
+    public class Mapping
+    {
+        public string MapName { get; set; } = string.Empty;
+        public List<(long, long, long)> Ranges { get; set; } = [];
+    }
+}
---
+++ 2023/5/Program.cs
@@ -0,0 +1,129 @@
+﻿using _5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sr = new StreamReader("../../../input.txt");
+        var line = sr.ReadLine();
+        var seedNumbers = line.Split(':')[1].Trim();
+        List<long> seeds = seedNumbers.Split(' ').Select(long.Parse).ToList();
+        line = sr.ReadLine();
+        // Placeholder for current mapping, will update through iteration
+        var currentMap = new Mapping();
+        // All mappings in a list for iteration
+        var mappings = new List<Mapping>();
+        while (line != null)
+        {
+            if (line != string.Empty)
+            {
+                var mapSpecification = line.Split(' ');
+                // If destination range start, source range start and range
+                if (mapSpecification.Length == 3)
+                {
+                    currentMap.Ranges.Add((long.Parse(mapSpecification[0]), long.Parse(mapSpecification[1]), long.Parse(mapSpecification[2])));
+                }
+                // Else it is a name/header for the mapping
+                else
+                {
+                    currentMap = new Mapping { MapName = line };
+                }
+            }
+            // If the line is empty the collection of mappings is done, add the currentMap to mappings.
+            else
+            {
+                if (currentMap.MapName != null)
+                {
+                    mappings.Add(currentMap);
+                }
+            }
+            line = sr.ReadLine();
+        }
+        // Do not miss the last "currentMap" after iteration
+        mappings.Add(currentMap);
+
+        // Part 1
+        List<long> locations = [];
+        foreach (var seed in seeds)
+        {
+            locations.Add(FindLocation(seed, mappings));
+        }
+        var shortestLocationPart1 = locations.Min();
+        Console.WriteLine($"Part 1 --> Lowest location is: {shortestLocationPart1}.");
+
+        // Part 2 || Slow! ~15-20 min.
+        long shortestLocationPart2 = FindPart2Location(seeds, mappings);
+        Console.WriteLine($"Part 2 --> Lowest location is: {shortestLocationPart2}.");
+    }
+
+    /// <summary>
+    /// Find the end location for a seed based on all mappings
+    /// </summary>
+    /// <param name="seed"></param>
+    /// <param name="mappings"></param>
+    /// <returns></returns>
+    private static long FindLocation(long seed, List<Mapping> mappings)
+    {
+        long currentLocation = seed;
+        foreach (var mapping in mappings)
+        {
+            foreach (var range in mapping.Ranges)
+            {
+                if (currentLocation >= range.Item2 && currentLocation <= range.Item2 + range.Item3)
+                {
+                    var difference = range.Item1 - range.Item2;
+                    currentLocation += difference;
+                    break;
+                }
+            }
+        }
+
+        return currentLocation;
+    }
+
+    /// <summary>
+    /// Slow!!!
+    /// With the seeds list provided loop through all seeds and find location,
+    /// return the shortest location.
+    /// </summary>
+    /// <param name="seeds"></param>
+    /// <param name="mappings"></param>
+    /// <returns></returns>
+    private static long FindPart2Location(List<long> seeds, List<Mapping> mappings)
+    {
+        // Initiate shortest location as max value.
+        long shortestLocation = long.MaxValue;
+        var seedCouples = new List<Tuple<long, long>>();
+        // Every second (1,3,5...) seed number is the start of a range of seed numbers.
+        // Every second (2,4,6...) seed number is now the size of the range of seed numbers
+        // following the first seed number.
+        for (int i = 0; i < seeds.Count; i += 2)
+        {
+            var seed = new Tuple<long, long>(seeds[i], seeds[i + 1]);
+            seedCouples.Add(seed);
+        }
+        // For every seed couple
+        foreach (var seedCouple in seedCouples)
+        {
+            // Generate the seed number based on initial seed number
+            // added by every number in the range.
+            for (int i = 0; i < seedCouple.Item2; i++)
+            {
+                long seed = seedCouple.Item1 + i;
+                long location = long.MaxValue;
+                // Find the location of the generated seed
+                location = FindLocation(seed, mappings);
+                // If the location is shorter than the already shortest location, take that
+                if (location < shortestLocation)
+                {
+                    shortestLocation = location;
+                }
+            }
+        }
+
+        return shortestLocation;
+    }
+}
+
+// Part 1 --> 57075758
+// Part 2 --> 31161857
