a33ce6a53de09f866c211b8204769b3c8bfc2855
Erik Blücher <erik.blucher@gmail.com>
2023-12-06 21:38:32 +0100

    2023: Day 5. Throw parallelism on the problem instead of fixing it for real.

--- 2023/5/Program.cs
+++ 2023/5/Program.cs
@@ -51,30 +51,42 @@ internal class Program
         var shortestLocationPart1 = locations.Min();
         Console.WriteLine($"Part 1 --> Lowest location is: {shortestLocationPart1}.");
 
-        // Part 2 || Slow! ~15-20 min.
+        // Part 2 || Slow! ~10 min.
         long shortestLocationPart2 = FindPart2Location(seeds, mappings);
         Console.WriteLine($"Part 2 --> Lowest location is: {shortestLocationPart2}.");
     }
 
     /// <summary>
-    /// Find the end location for a seed based on all mappings
+    /// Find the end location for a seed based on all mappings.
     /// </summary>
-    /// <param name="seed"></param>
+    /// <param name="currentLocation"></param>
     /// <param name="mappings"></param>
     /// <returns></returns>
-    private static long FindLocation(long seed, List<Mapping> mappings)
+    private static long FindLocation(long currentLocation, List<Mapping> mappings)
     {
-        long currentLocation = seed;
         foreach (var mapping in mappings)
         {
-            foreach (var range in mapping.Ranges)
+            currentLocation = NextLocation(currentLocation, mapping);
+        }
+
+        return currentLocation;
+    }
+
+    /// <summary>
+    /// Find the next location from the current location and mapping.
+    /// </summary>
+    /// <param name="currentLocation"></param>
+    /// <param name="mapping"></param>
+    /// <returns></returns>
+    private static long NextLocation(long currentLocation, Mapping mapping)
+    {
+        foreach (var range in mapping.Ranges)
+        {
+            if (currentLocation >= range.Item2 && currentLocation <= range.Item2 + range.Item3)
             {
-                if (currentLocation >= range.Item2 && currentLocation <= range.Item2 + range.Item3)
-                {
-                    var difference = range.Item1 - range.Item2;
-                    currentLocation += difference;
-                    break;
-                }
+                var difference = range.Item1 - range.Item2;
+                currentLocation += difference;
+                break;
             }
         }
 
@@ -91,8 +103,6 @@ internal class Program
     /// <returns></returns>
     private static long FindPart2Location(List<long> seeds, List<Mapping> mappings)
     {
-        // Initiate shortest location as max value.
-        long shortestLocation = long.MaxValue;
         var seedCouples = new List<Tuple<long, long>>();
         // Every second (1,3,5...) seed number is the start of a range of seed numbers.
         // Every second (2,4,6...) seed number is now the size of the range of seed numbers
@@ -102,22 +112,39 @@ internal class Program
             var seed = new Tuple<long, long>(seeds[i], seeds[i + 1]);
             seedCouples.Add(seed);
         }
-        // For every seed couple
-        foreach (var seedCouple in seedCouples)
+
+        List<long> shortestLocationsPerSeedCouple = [];
+        Parallel.ForEach(seedCouples, seedCouple =>
         {
-            // Generate the seed number based on initial seed number
-            // added by every number in the range.
-            for (int i = 0; i < seedCouple.Item2; i++)
+            long shortestLocationPerSeedCouple = FindShortestLocationPerSeedCouple(seedCouple, mappings);
+            shortestLocationsPerSeedCouple.Add(shortestLocationPerSeedCouple);
+        });
+
+        return shortestLocationsPerSeedCouple.Min();
+    }
+
+    /// <summary>
+    /// Find the shortest location for a seed couple through all mapping.
+    /// </summary>
+    /// <param name="seedCouple"></param>
+    /// <param name="mappings"></param>
+    /// <returns></returns>
+    private static long FindShortestLocationPerSeedCouple(Tuple<long, long> seedCouple, List<Mapping> mappings)
+    {
+        long shortestLocation = long.MaxValue;
+
+        // Generate the seed number based on initial seed number
+        // added by every number in the range.
+        for (int i = 0; i < seedCouple.Item2; i++)
+        {
+            long seed = seedCouple.Item1 + i;
+            long location = long.MaxValue;
+            // Find the location of the generated seed
+            location = FindLocation(seed, mappings);
+            // If the location is shorter than the already shortest location, take that
+            if (location < shortestLocation)
             {
-                long seed = seedCouple.Item1 + i;
-                long location = long.MaxValue;
-                // Find the location of the generated seed
-                location = FindLocation(seed, mappings);
-                // If the location is shorter than the already shortest location, take that
-                if (location < shortestLocation)
-                {
-                    shortestLocation = location;
-                }
+                shortestLocation = location;
             }
         }
 
