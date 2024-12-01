85e83ea093e49874f8f629634f2b1b6a8ea52268
Erik Blücher <erik.blucher@gmail.com>
2024-12-01 11:07:48 +0100

    2024: Day 1

---
+++ 2024/1/Program.cs
@@ -0,0 +1,65 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sr = new StreamReader("../../../input.txt");
+        var line = sr.ReadLine();
+        List<int> leftListLocations = [];
+        List<int> rightListLocations = [];
+        while (line != null)
+        {
+            var values = line.Split(' ').Where(l => l != string.Empty).ToList();
+            leftListLocations.Add(int.Parse(values[0]));
+            rightListLocations.Add(int.Parse(values[1]));
+            line = sr.ReadLine();
+        }
+        leftListLocations = [.. leftListLocations.OrderBy(l => l)];
+        rightListLocations = [.. rightListLocations.OrderBy(l => l)];
+
+        // Part 1
+        int distance = GetDistance(leftListLocations, rightListLocations);
+        Console.WriteLine(distance);
+
+        // Part 2
+        int similarityScore = GetSimilarityScore(leftListLocations, rightListLocations);
+        Console.WriteLine(similarityScore);
+    }
+
+    /// <summary>
+    /// Get the sum of distances of ordered left and right locations.
+    /// </summary>
+    /// <param name="leftListLocations"></param>
+    /// <param name="rightListLocations"></param>
+    /// <returns></returns>
+    private static int GetDistance(List<int> leftListLocations, List<int> rightListLocations)
+    {
+        List<int> distances = [];
+        for (int i = 0; i < leftListLocations.Count; i++)
+        {
+            distances.Add(Math.Abs(leftListLocations[i] - rightListLocations[i]));
+        }
+
+        int distance = distances.Sum();
+        return distance;
+    }
+
+    /// <summary>
+    /// Get the similarity score of the left and right locations.
+    /// </summary>
+    /// <param name="leftListLocations"></param>
+    /// <param name="rightListLocations"></param>
+    /// <returns></returns>
+    private static int GetSimilarityScore(List<int> leftListLocations, List<int> rightListLocations)
+    {
+        int score = 0;
+        List<int> scores = [];
+        foreach (int leftLocation in leftListLocations)
+        {
+            var numberOfOccurences = rightListLocations.Where(rightLocation => rightLocation == leftLocation).Count();
+            scores.Add(leftLocation * numberOfOccurences);
+        }
+
+        score = scores.Sum();
+        return score;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Advent of code
 
-Starting with Advent of Code 2023. Some puzzles from earlier years are on an old computer.
+Starting with Advent of Code 2023.
+Now also 2024 started!
+Let´s see how far I'll go and if I will finish 2023 one day.
 
 ### Technology
 Console applications that outputs the results based on the supplied input.<br>
